fb8f1647ed2b1e736794fc340d95414a9a5aaf9e
Travis Person <travis.person@gmail.com>
2013-10-19 14:01:04 -0700

    Seperated out some info to other files

--- src/packetUtils.go
+++ src/packetUtils.go
@@ -4,43 +4,23 @@ import (
 	"net"
 	"os"
 	"encoding/gob"
+	"time"
 )
 
 type connecInfo struct {
 	ipaddr string //port and addr
 }
 
-/* packet interface */
-type packet interface { }
-
-/* packet type 1, containing client info */
-type clientPacket struct {
-	ipaddr string
-}
-
-/* packet type 2, containing info on all files */
-type serverFileTree struct {
-	files []serverFiles
-}
-
 /* file info */
-type file struct {
-	string modTime
-	string hash
-	string path
-}
 
 func getPacket (conn net.Conn) {
 	decode = gob.NewDecoder(conn)
 	id := 0
-	fmt.Fscanf(conn, "%d", &id)
 
 	var p packet
-	switch (id) {
-		case 1: 
-			decode.Decode(&p)
-			p.handleInfo()
-	}
+	decode.Decode(&p)
+	p.handleInfo()
+
 }
 
 func idPacket(id int, pkt packet) {
